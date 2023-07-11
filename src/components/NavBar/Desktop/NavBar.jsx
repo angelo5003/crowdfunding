8e7255d447e020d816dbb2ed278171b61e3b6dc0
Tony5300 <tony_tolen@ziggo.nl>
2023-07-11 17:48:47 +0200

    feat: created the desktop and mobile navbar

--- src/components/NavBar/Desktop/NavBar.jsx
+++ src/components/NavBar/Desktop/NavBar.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import logo from "../../../assets/images/logo.svg";
 import MobileMenu from "../../../assets/images/icon-hamburger.svg";
 import MobileCloseIcon from "../../../assets/images/icon-close-menu.svg";
+import MobileNav from "../Mobile/MobileNav";
 
 const NavBar = () => {
   const [mobileMenu, setMobileMenu] = useState(false);
@@ -11,28 +12,37 @@ const NavBar = () => {
     setMobileMenu(!mobileMenu);
   };
   return (
-    <header className="nav-outer-container">
-      <nav className="nav-inner-container">
-        <img src={logo} alt="logo" />
-        <ul className="nav-links-container">
-          <li className="nav-link">About</li>
-          <li className="nav-link">Discover</li>
-          <li className="nav-link">Get Started</li>
-        </ul>
+    <div className="test">
+      <header className="nav-outer-container">
+        <nav className="nav-inner-container">
+          <img src={logo} alt="logo" />
+          <ul className="nav-links-container">
+            <li className="nav-link">About</li>
+            <li className="nav-link">Discover</li>
+            <li className="nav-link">Get Started</li>
+          </ul>
 
-        <div className="nav-mobile-menu-btn-container">
-          {mobileMenu ? (
-            <button onClick={handleShowMobileMenu} className="mobile-menu-btn">
-              <img src={MobileCloseIcon} alt="mobile-close-icon" />
-            </button>
-          ) : (
-            <button onClick={handleShowMobileMenu} className="mobile-menu-btn">
-              <img src={MobileMenu} alt="mobile-menu" />
-            </button>
-          )}
-        </div>
-      </nav>
-    </header>
+          <div className="nav-mobile-menu-btn-container">
+            {mobileMenu ? (
+              <button
+                onClick={handleShowMobileMenu}
+                className="mobile-menu-btn"
+              >
+                <img src={MobileCloseIcon} alt="mobile-close-icon" />
+              </button>
+            ) : (
+              <button
+                onClick={handleShowMobileMenu}
+                className="mobile-menu-btn"
+              >
+                <img src={MobileMenu} alt="mobile-menu" />
+              </button>
+            )}
+          </div>
+          {mobileMenu ? <MobileNav /> : null}
+        </nav>
+      </header>
+    </div>
   );
 };
 
