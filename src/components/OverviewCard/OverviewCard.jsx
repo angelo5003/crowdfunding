51b3a106d8db20432d9d62f55214397248b61f5c
Tony5300 <tony_tolen@ziggo.nl>
2023-07-13 17:22:59 +0200

    style: styled the results card and created the progressbar

--- src/components/OverviewCard/OverviewCard.jsx
+++ src/components/OverviewCard/OverviewCard.jsx
@@ -1,16 +1,27 @@
+import ProgressBar from "../ProgressBar/ProgressBar";
 import "./OverviewCardStyle.css";
 
 const OverviewCard = () => {
   return (
-    <article className="overview-card-container">
-      <h2>$89,914</h2>
-      <p>of $100,000 backed</p>
-      <h2>5,007</h2>
-      <p>total backers</p>
-      <h2>56</h2>
-      <p>days left</p>
-      <div>Progress bar</div>
-    </article>
+    <div className="overview-card-outer-container">
+      <article className="overview-card-container">
+        <div className="overview-results">
+          <h2 className="overview-result-title">$89,914</h2>
+          <p className="overview-result-info">of $100,000 backed</p>
+        </div>
+        <hr className="overview-result-vertical-line" />
+        <div className="overview-results">
+          <h2 className="overview-result-title">5,007</h2>
+          <p className="overview-result-info">total backers</p>
+        </div>
+        <hr className="overview-result-vertical-line" />
+        <div className="overview-results">
+          <h2 className="overview-result-title">56</h2>
+          <p className="overview-result-info">days left</p>
+        </div>
+      </article>
+      <ProgressBar />
+    </div>
   );
 };
 
