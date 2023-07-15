1f8478f581f379fabb6955a12177fa0064c33823
Tony5300 <tony_tolen@ziggo.nl>
2023-07-15 09:23:17 +0200

    style: styled the about page component for dekstop view

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import "./HomeStyles.css";
 import ProductTitleCard from "../components/ProductTitleCard/ProductTitleCard";
 import OverviewCard from "../components/OverviewCard/OverviewCard";
+import About from "./About/About";
 
 const Home = () => {
   return (
     <div className="home-page-outer-container">
       <ProductTitleCard />
       <OverviewCard />
+      <About />
     </div>
   );
 };
