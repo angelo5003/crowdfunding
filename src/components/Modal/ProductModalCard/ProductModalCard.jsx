9c062b4cc7d38927a1e419dc2cef3c19d633c96a
Tony5300 <tony_tolen@ziggo.nl>
2023-07-27 17:38:06 +0200

    feat: created the radio btn and selection

--- src/components/Modal/ProductModalCard/ProductModalCard.jsx
+++ src/components/Modal/ProductModalCard/ProductModalCard.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import "./ProductModalCardStyles.css";
 import ModalCloseBtn from "../ModalCloseBtn/ModalCloseBtn";
 import { ShowProductModalPropTypes } from "../../../utils/PropTypes";
 import backerData from "../../../data/backerData.json";
 
 const ProductModalCard = ({ handleShowProductModal }) => {
+  const [selectedProduct, setSelectedProduct] = useState(""); // keep track of the currently selected radio btn. The empty string is to make sure that the default value is empty
+  const handleInputChange = (e) => {
+    const { name } = e.target;
+    setSelectedProduct(name);
+    console.log(`clicked on the:`, name);
+  };
   return (
     <>
       <section className="modal-outer-container">
@@ -18,7 +25,13 @@ const ProductModalCard = ({ handleShowProductModal }) => {
             return (
               <div className="modal-details-outer-container" key={data.id}>
                 <div className="modal-details-inner-container">
-                  <input type="radio" />
+                  <input
+                    type="radio"
+                    name="backerOption" // use the same name for all radio btn in the group
+                    value={data.id} // set the value of the radio btn to data.id
+                    checked={selectedProduct === data.id} // check if the radio btn is selected
+                    onChange={handleInputChange}
+                  />
                   <h3 className="modal-detail-title">{data.backerTitle}</h3>
                   <h3 className="modal-detail-title">
                     Pledge ${data.backerPledge} or more
