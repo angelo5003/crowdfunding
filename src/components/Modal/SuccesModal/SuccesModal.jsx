8c62ff91f166a4adce62923da318a7e1ebb7ee39
Tony5300 <tony_tolen@ziggo.nl>
2023-08-02 16:54:34 +0200

    feat: created the modal when you click on the select reward btn

---
+++ src/components/Modal/SuccesModal/SuccesModal.jsx
@@ -0,0 +1,30 @@
+import "./SuccesModalStyles.css";
+import { Portal } from "react-portal";
+import SuccesLogo from "../../../assets/images/icon-check.svg";
+
+const SuccesModal = () => {
+  return (
+    <>
+      <Portal>
+        <div className="succes-modal-background">
+          <div className="succes-modal-outer-container">
+            <img
+              src={SuccesLogo}
+              alt="succes-logo"
+              className="succes-modal-logo"
+            />
+            <h2 className="succes-modal-title">Thanks for your support</h2>
+            <p className="succes-modal-message">
+              Thanks for your support! Your pledge brings us one step closer to
+              sharing Mastercraft Bamboo Monitor Riser worldwide. You will get
+              an email once our campaign is completed.
+            </p>
+            <button className="succes-modal-btn">Got It!</button>
+          </div>
+        </div>
+      </Portal>
+    </>
+  );
+};
+
+export default SuccesModal;
--- src/components/PledgeDropDownBox/PledgeDropDownBox.jsx
+++ src/components/PledgeDropDownBox/PledgeDropDownBox.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+import SuccesModal from "../Modal/SuccesModal/SuccesModal";
 import "./PledgeDropDownBoxStyles.css";
 import { LiaDollarSignSolid } from "react-icons/lia";
 
 const PledgeDropDownBox = () => {
+  const [showSuccesModal, setShowSuccesModal] = useState(false);
+
+  const handleShowSuccesModal = () => {
+    setShowSuccesModal(!showSuccesModal);
+    console.log(`you clicked on the continue button`);
+  };
   return (
     <section className="dropdown-pledge-outer-container">
       <hr className="dropdown-pledge-hr-line" />
@@ -9,10 +17,16 @@ const PledgeDropDownBox = () => {
         <p className="dropdown-pledge-title">Enter your pledge</p>
         <div className="dropdown-pledge-controls">
           <span className="dropdown-pledge-input-container">
-            <LiaDollarSignSolid />
+            <LiaDollarSignSolid className="dropdown-pledge-dollar-sign" />
             <input type="text" className="dropdown-pledge-input" />
           </span>
-          <button className="dropdown-pledge-btn">Continue</button>
+          <button
+            className="dropdown-pledge-btn"
+            onClick={handleShowSuccesModal}
+          >
+            Continue
+          </button>
+          {showSuccesModal ? <SuccesModal /> : null}
         </div>
       </div>
     </section>
